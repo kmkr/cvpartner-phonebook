53dd5314aeed42a11e0caae10b967ffcbb960c1e
Kris-Mikael Krister <kris-mikael.krister@webstep.no>
2014-05-27 20:05:28 +0200

    svg works

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.get('/api/users', function(req, res) {
     });
 });
 app.get('/api/categories/:category', function(req, res) {
-    fetch('api/v1/search?query[0]=' + req.params.category + '&filter_fields[0]=_all&office_ids[]=52726286e79c697aa4000001&office_ids[]=51a6755f6fc6269990000001&office_ids[]=4f6074ee875b52672f000001&office_ids[]=527261e1e79c69b457000001&office_ids[]=52726121e79c6997ab000001&office_ids[]=52725fc2e79c69d555000001&office_ids[]=52726249e79c69c572000001&office_ids[]=52f16e122f274f8aa0000001&office_ids[]=5272614ce79c69e986000001&office_ids[]=51a675d46fc626e7f8000001&office_ids[]=51a675926fc626fead000001&size=100').then(function(data) {
-        res.send(JSON.stringify(data));
+    fetch('/api/v1/search?query[0]=' + req.params.category + '&filter_fields[0]=_all&office_ids[]=52726286e79c697aa4000001&office_ids[]=51a6755f6fc6269990000001&office_ids[]=4f6074ee875b52672f000001&office_ids[]=527261e1e79c69b457000001&office_ids[]=52726121e79c6997ab000001&office_ids[]=52725fc2e79c69d555000001&office_ids[]=52726249e79c69c572000001&office_ids[]=52f16e122f274f8aa0000001&office_ids[]=5272614ce79c69e986000001&office_ids[]=51a675d46fc626e7f8000001&office_ids[]=51a675926fc626fead000001&size=1').then(function(data) {
+        res.send(JSON.stringify(data.total));
     });
 });
 app.use(express.static(__dirname + '/src'));
@@ -38,11 +38,11 @@ console.log('Started HTTP server on port 8080');
 
 
 var fetch = function(url) {
-    console.log('Fetching production data');
+    console.log('Fetching production data from ' + url);
     var requestOpts = {
         hostname: process.env.CVPARTNER_HOSTNAME,
         port: 443,
-        path: '/api/v1/users',
+        path: url,
         method: 'GET',
         headers: {
             Authorization: 'Token token="' + process.env.CVPARTNER_TOKEN + '"'
@@ -56,6 +56,7 @@ var fetch = function(url) {
         });
 
         res.on('end', function() {
+            console.log(data);
             deferred.resolve(JSON.parse(data));
         });
     });
@@ -67,20 +68,25 @@ var fetch = function(url) {
     return deferred.promise;
 };
 
-var fetchMockUsers = function() {
+var fetchMock = function(url) {
     console.log('Fetching mock data');
+
     var deferred = q.defer();
-    deferred.resolve([
-        { name: 'Borghild Balder', email: 'Borghild.Balder@foo.com', telephone: '480 47 878' },
-        { name: 'Gandalf Ask', email: 'Gandalf.Ask@foo.com', telephone: '901 52 711' },
-        { name: 'Hel Gerd', email: 'Hel.Gerd@foo.com', telephone: '98665838' },
-        { name: 'Thor Vidar', email: 'Thor.Vidar@foo.com', telephone: '' },
-        { name: 'Sif Oden', email: 'Sif.Oden@foo.com', telephone: '+47 926 17 422' },
-        { name: 'Sigurd Borghildr', email: 'Sigurd.Borghildr@foo.com', telephone: '+47-92663081' }
-    ]);
+    if (url.match(/users/)) {
+        deferred.resolve([
+            { name: 'Borghild Balder', email: 'Borghild.Balder@foo.com', telephone: '480 47 878' },
+            { name: 'Gandalf Ask', email: 'Gandalf.Ask@foo.com', telephone: '901 52 711' },
+            { name: 'Hel Gerd', email: 'Hel.Gerd@foo.com', telephone: '98665838' },
+            { name: 'Thor Vidar', email: 'Thor.Vidar@foo.com', telephone: '' },
+            { name: 'Sif Oden', email: 'Sif.Oden@foo.com', telephone: '+47 926 17 422' },
+            { name: 'Sigurd Borghildr', email: 'Sigurd.Borghildr@foo.com', telephone: '+47-92663081' }
+        ]);
+    } else {
+        deferred.resolve(3);
+    }
     return deferred.promise;
 };
 
 if ('dev' === process.argv[2]) {
-    fetch = fetchMockUsers;
+    fetch = fetchMock;
 }
--- src/js/categoriesController.js
+++ src/js/categoriesController.js
@@ -1,6 +1,27 @@
 angular.module('phonebook')
 .controller('CategoriesController', ['$scope', 'DataService', function($scope, DataService) {
+	$scope.data = [];
 	$scope.search = function () {
-		DataService.fetchCategory($scope.searchText);
+		var txt = $scope.searchText;
+		var data = [];
+		DataService.fetchCategory(txt).then(function (length) {
+			data.push({
+				name: txt,
+				num: length
+			});
+
+			var vis=d3.select(".chart").append("svg:svg").attr("width",100).attr("height",700);
+			var rect=vis.selectAll("rect").data(data).enter().append("svg:rect");
+			rect.attr("height",function(d) {
+				return d.num;
+			})
+			  .attr("width", 15)
+			  .attr("x",function(d,i) {
+			  	return i*20;
+			  })
+			  .attr("y",function(d) {return 0;})
+			  .attr("fill","steelblue")
+			  .text(function(d) { return d.name; });
+		});
 	};
 }]);
